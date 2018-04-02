675c07ac77b0591c4c36091bac30209c8baf7022
yl2014 <842237798@qq.com>
2018-04-02 13:01:53 +0800

    test: jenkinsfile

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
-if [ ! -d "/tmp/github" ]
-  mkdir /tmp/github
-  chmod -R 777 /tmp/github
+if [ ! -d "$JENKINS_HOME/tmp/github" ]
+  mkdir $JENKINS_HOME/tmp/github
+  chmod -R 777 $JENKINS_HOME/tmp/github
 fi
 
-cp -R ./public/ /tmp/github
+cp -R ./public/ $JENKINS_HOME/tmp/github
 
-if [ ! -d "/tmp/YL2014.github.io" ]
+if [ ! -d "$JENKINS_HOME/tmp/YL2014.github.io" ]
   git clone git@github.com:YL2014/YL2014.github.io.git
-  chmod -R 777 /tmp/YL2014.github.io
+  chmod -R 777 $JENKINS_HOME/tmp/YL2014.github.io
 fi
 
-cd /tmp/YL2014.github.io
+cd $JENKINS_HOME/tmp/YL2014.github.io
 
 git pull origin master
 
-cp -R /tmp/github/ /tmp/YL2014.github.io
+cp -R $JENKINS_HOME/tmp/github/ $JENKINS_HOME/tmp/YL2014.github.io
 
 git add . && git commit -am "add articles" && git push origin master
